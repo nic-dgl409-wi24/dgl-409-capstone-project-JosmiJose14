92d3378cf6267be61dc3a0e4c1173c78d2a7c705
Josmi Jose <josmijose100@gmail.com>
2024-02-19 13:36:08 -0800

    Change the page name Home

--- unitystock-hub/src/App.js
+++ unitystock-hub/src/App.js
@@ -3,15 +3,15 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 
 import Header from './pages/Header';
 import Footer from './pages/Footer';
-import Landing from './pages/Landing';
+import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Navigate to="/Landing" replace />} />
-        <Route path="/Landing" element={<Landing />} />
+        <Route path="/" element={<Navigate to="/Home" replace />} />
+        <Route path="/Home" element={<Home />} />
         {/* Uncomment and create the following components when needed */}
         {/* <Route path="/Service" element={<Service />} />
         <Route path="/Appointments" element={<Appointments />} /> */}
