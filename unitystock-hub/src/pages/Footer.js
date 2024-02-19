1eb811f62a93f708a3ab0c91a9c6fe3c699ffa8e
Josmi Jose <josmijose100@gmail.com>
2024-02-19 13:33:20 -0800

    Add footer and adjusted the style for all windows

--- unitystock-hub/src/pages/Footer.js
+++ unitystock-hub/src/pages/Footer.js
@@ -1,41 +1,37 @@
 import React from 'react';
-import { FaEnvelope, FaMobile, FaHome } from 'react-icons/fa';
+import { FaInstagram, FaLinkedinIn, FaFacebookF } from 'react-icons/fa';
+import '../css/Footer.css'; // Ensure the path is correct for your project structure
 
 function Footer() {
   return (
-    <div id="divfooter">
+    <footer className="footer">
+    
       <div className="footerAboutUS">
         <div className="column">
-          <h3 className="footerHeading">Services</h3>
+          <h3 className="footerHeading">Unitystock Hub</h3>
           <div className="footerSub">
-            <p>Hair Examination and Remedies</p>
-            <p>Gender-free haircuts</p>
-            <p>Hair Treatments</p>
-            <p>Transform my feel</p>
+          <p>Streamlining Your Inventory, Simplifying Your Success!</p>
           </div>
         </div>
         <div className="column">
           <h3 className="footerHeading">Contact Us</h3>
           <div className="footerSub">
-            <p><FaEnvelope />&nbsp;hairwithflair@shaw.ca</p>
-            <p><FaMobile style={{ fontSize: '18px' }} />&nbsp;250-992-1858</p>
-            {/* <p><FaFax />&nbsp;250-992-1858</p> */}
-            <p><FaHome />&nbsp;xyz abc Road, Courtney, BC</p>
-            {/* <p><a href="" target="_blank"><FaFacebook /></a></p> */}
+            <p><FaInstagram style={{ fontSize: '18px' }} /></p>
+            <p><FaLinkedinIn style={{ fontSize: '18px' }} /></p>
+            <p><FaFacebookF /></p>
           </div>
         </div>
         <div className="column">
-          <h3 className="footerHeading">Business Hours</h3>
+          <h3 className="footerHeading">User Instructions</h3>
           <div className="footerSub">
-            <p>Tue-Sat: 10.00am - 5.00pm</p>
-            <p>Sun & Mon: Closed</p>
+          <p>User Manual</p>
           </div>
         </div>
       </div>
-      <footer>
-        <small><em>"This is a fictional website that was designed and coded as an educational exercise. It is not intended to be seen outside of the class environment. None of the content and images were approved by the business owner."</em></small>
-      </footer>
-    </div>
+      <div className="footer-bottom">
+      <small><em>"This is a fictional website that was designed and coded as an educational exercise. It is not intended to be seen outside of the class environment. None of the content and images were approved by the business owner."</em></small>
+      </div>
+    </footer>
   );
 }
 
