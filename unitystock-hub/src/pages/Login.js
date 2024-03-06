f92deee92b66044d0d5a49767c3f49ff0d0fca17
Josmi Jose <josmijose100@gmail.com>
2024-03-06 00:17:48 -0800

    Login page style changes

--- unitystock-hub/src/pages/Login.js
+++ unitystock-hub/src/pages/Login.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../css/Login.css'; // Ensure you have the CSS file for styling
+import '../css/Login.css';  //Ensure you have the CSS file for styling
 import loginImage from "../images/login-image.png";
 import config from '../common/config';
-import { useAuth } from '../pages/auth/AuthContext'; 
+import { useAuth } from '../pages/auth/AuthContext';
 import axios from 'axios';
 const LoginPage = () => {
     const navigate = useNavigate();
@@ -22,68 +22,67 @@ const LoginPage = () => {
         }));
     };
 
-// ...
-const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await axios.post(`${config.server.baseUrl}/login`, {
-        email: credentials.email, // Ensure the field names match the backend expectations
-        password: credentials.password
-      });
-      console.log(response.data);
-         // Check if the login is successful
-         
-         if (response.data.message === 'Login successful') {
-            login(response.data.user); //
-            navigate('/Division'); // Redirect on success
-          } else {
-            // Handle other responses
-          }
-    } catch (error) {
-      console.error('Login error', error.response);
-      // Handle errors here
-    }
-  };
-  // ...
-  
+    // ...
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.post(`${config.server.baseUrl}/login`, {
+                email: credentials.email, // Ensure the field names match the backend expectations
+                password: credentials.password
+            });
+            console.log(response.data);
+            // Check if the login is successful
 
+            if (response.data.message === 'Login successful') {
+                login(response.data.user); //
+                navigate('/Division'); // Redirect on success
+            } else {
+                // Handle other responses
+            }
+        } catch (error) {
+            console.error('Login error', error.response);
+            // Handle errors here
+        }
+    };
     return (
         <div className="login">
-            <h2 className='section-heading'>Welcome Back</h2> {/* Heading for the form */}
-            <div className="login-container">
-                <div className="login-info">
-                    <img
-                        className="login-image"
-                        src={loginImage} // Placeholder image path
-                        alt="Login"
-                    />
+            <div className="login-page">
+                <div className="left-panel">
+                    <div className="content">
+                        <h1 className='section-heading'>UnityStock Hub</h1>
+                        <p>Keep your inventory organized and accessible. Log in to manage your stock efficiently.</p>
+                        <div className="login-info">
+                            <img className="login-image" src={loginImage} alt="Login" />
+                        </div>
+
+
+                    </div>
                 </div>
                 <div className="login-form">
-                    <p>Please sign in to access your inventory management dashboard and streamline your business operations. Manage your inventory with ease and efficiency. Let's get started</p>
-                    <form onSubmit={handleSubmit}>
-                <label htmlFor="email">Email</label>
-                <input
-                    id="email"
-                    type="email"
-                    name="email"
-                    value={credentials.email} // Bind input to state
-                    placeholder="Email"
-                    onChange={handleChange} // Set up change handler
-                />
-                <label htmlFor="password">Password</label>
-                <input
-                    id="password"
-                    type="password"
-                    name="password"
-                    value={credentials.password} // Bind input to state
-                    placeholder="Password"
-                    onChange={handleChange} // Set up change handler
-                />
-
-                <div className="divButton">
-                    <button type="submit" className='btn btnLogin'>Login</button>
-                </div>
-            </form>
+                    <form onSubmit={handleSubmit} className="login-form">
+                        <h2 className='section-heading'>Welcome Back</h2> {/* Heading for the form */}
+                        <label htmlFor="email">Email</label>
+                        <input
+                            id="email"
+                            type="email"
+                            name="email"
+                            value={credentials.email}
+                            placeholder="Email"
+                            onChange={handleChange}
+                        />
+                        <label htmlFor="password">Password</label>
+                        <input
+                            id="password"
+                            type="password"
+                            name="password"
+                            value={credentials.password}
+                            placeholder="Password"
+                            onChange={handleChange}
+                        />
+                        <div className="divButton">
+                            <button type="submit" className="btn btnLogin">Login</button>
+                        </div>
+                    </form>
                 </div>
             </div>
             <div className="join-container">
