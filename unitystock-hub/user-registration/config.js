29753a3f1955b45e1355872ba56d1498edf02d6b
Josmi Jose <josmijose100@gmail.com>
2024-03-17 12:59:43 -0700

    Add config file for the server

---
+++ unitystock-hub/user-registration/config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  // Server configuration
+  PORT: process.env.PORT || 3001,
+
+  // Database configuration
+  dbConfig: {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'user_db'
+  },
+
+  // Google Sheets API configuration
+  googleSheetsConfig: {
+    spreadsheetId: '1-GYq6o3zJ_MlMCqHCmU9XRFyVCyi2gRx8e-kkN2DIaI',
+    subspreadsheetId: '12_hbBZt7NU8nj-JfInDxLMvBjpYps82Vw4k2-SHjEXU',
+    inventoryspreadsheetId: '1hO7IajrBcAypA8FKjngv9G65ltq8Uw3VV-EFATdJlZI',
+    keyFile: path.join(__dirname, 'unitystock-hub-google.json'), // Path to your service account credentials
+    scopes: ['https://www.googleapis.com/auth/spreadsheets']
+  },
+
+  // Static files configuration
+  staticFilesConfig: {
+    division: '/images/division',
+    subdivision: '/images/subdivision',
+    profile: '/images/profile',
+    inventory: '/images/inventory'
+  }
+};
